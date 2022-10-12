1f0b95cf3ee8b4523974b2a8f7d27ad2f399a689
ottagit <ottaosawa@gmail.com>
2022-10-12 15:04:57 +0000

    create a LAMP system configuration

---
+++ manifests/lamp.pp
@@ -0,0 +1,9 @@
+# @summary A Configures a whole LAMP stack
+#
+# @example
+#   include role::lamp
+class role::lamp {
+  include profile::base
+  include profile::apache
+  include profile::mysql::server
+}
